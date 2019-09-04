48d27cb82d91fe49f55f3a5aab44bce1d9435881
Ryan Ma <ryama@ucdavis.edu>
2019-09-03 19:45:41 -0700

    Updates

---
+++ precache-manifest.392902efe8d1be7508ec5ff0cc7eb8e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ccbd64e54e3ddfda6f9226d0d418810",
+    "url": "/RyanMa1/react-clothing-store-app.github.io/hello/index.html"
+  },
+  {
+    "revision": "364b6ec1cf16be67ebbc",
+    "url": "/RyanMa1/react-clothing-store-app.github.io/hello/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "001e914de745a4058303",
+    "url": "/RyanMa1/react-clothing-store-app.github.io/hello/static/css/main.66c8a017.chunk.css"
+  },
+  {
+    "revision": "364b6ec1cf16be67ebbc",
+    "url": "/RyanMa1/react-clothing-store-app.github.io/hello/static/js/2.c5db4317.chunk.js"
+  },
+  {
+    "revision": "001e914de745a4058303",
+    "url": "/RyanMa1/react-clothing-store-app.github.io/hello/static/js/main.9ef67365.chunk.js"
+  },
+  {
+    "revision": "6d02dddedc7eda0d5453",
+    "url": "/RyanMa1/react-clothing-store-app.github.io/hello/static/js/runtime~main.ded7fe4e.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/RyanMa1/react-clothing-store-app.github.io/hello/static/media/logo.9f7e506d.svg"
+  }
+]);
